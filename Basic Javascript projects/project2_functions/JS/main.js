747c37ac039761bea673e5bcd12f14d0f1760351
MoksiPate <MoksiPatel@github.com>
2023-06-11 14:51:50 -0400

    commented code

--- Basic Javascript projects/project2_functions/JS/main.js
+++ Basic Javascript projects/project2_functions/JS/main.js
@@ -1,7 +1,7 @@
- 
+      //declare function name
       function demo() { 
-        var x=" to learn more subscribe"
-        var y=" at aolcc@aolcc.com"
-         document.getElementById("result").innerHTML = x + y; 
+        var x=" to learn more subscribe"  //variable name and quoated for string
+        var y=" at aolcc@aolcc.com"       //string varibale
+         document.getElementById("result").innerHTML = x + y; // method with ID and operator to conctanate to varibales
       } 
    
