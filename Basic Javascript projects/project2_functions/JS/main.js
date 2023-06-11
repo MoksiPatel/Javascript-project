54bb241bb9830904c4abcdea525529c5ed360d1b
MoksiPate <MoksiPatel@github.com>
2023-06-11 14:48:56 -0400

    updates

---
+++ Basic Javascript projects/project2_functions/JS/main.js
@@ -0,0 +1,7 @@
+ 
+      function demo() { 
+        var x=" to learn more subscribe"
+        var y=" at aolcc@aolcc.com"
+         document.getElementById("result").innerHTML = x + y; 
+      } 
+   
