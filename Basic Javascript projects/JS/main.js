cc02d38994635acf7d10126cf23efcad62bb583c
MoksiPate <MoksiPatel@github.com>
2023-06-11 09:56:43 -0400

    Updated code

---
+++ Basic Javascript projects/JS/main.js
@@ -0,0 +1,5 @@
+window.alert("click to learn");  //it will add alert on windows with button to click
+var sent1="this is first part of statement"; //variable declaration
+var sent2=" and this is second part of statement."; // variable declaration
+document.write(sent1 + sent2); //this method will concatanate two string variables and write on webpage with document.write statement
+document.write(3 + 19);  //this is expression and will return addition result.
