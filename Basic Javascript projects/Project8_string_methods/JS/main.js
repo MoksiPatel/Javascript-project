77659dcdbe696d54145983b7a807065415dd476f
MoksiPate <MoksiPatel@github.com>
2023-06-21 03:55:53 -0400

    updated

--- Basic Javascript projects/Project8_string_methods/JS/main.js
+++ Basic Javascript projects/Project8_string_methods/JS/main.js
@@ -5,8 +5,28 @@ var partC="my diploma.";
 var complete_sentence=partA.concat(partB, partC);
 document.getElementById("Concatenate").innerHTML=complete_sentence;
 }
+
+//find position of string by slice method and make it uppercase
 function Slice_function() {
     var about="my name is Mayur and i am from India.";
     var name=about.slice(10,16);
-    document.getElementById("Slice").innerHTML=name;
+    var Uname=name.toUpperCase();
+    document.getElementById("Slice").innerHTML=Uname;   
+}
+//search india position
+let about="India is in Asia";
+let country=about.search("India");
+document.write(country);
+
+//convert number to string
+
+function method_string() {
+    var x=15;
+    document.getElementById("number_to_string").innerHTML=x.toString();
+}
+//precision method to round up number close to
+function precision_method(){
+    var y=123.2889890;
+    document.getElementById("Precision").innerHTML=y.toPrecision();
+
 }
